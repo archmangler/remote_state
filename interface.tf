fef9d7d363f728173db208eeffe358c1a069d078
Traiano Welcome <traiano@gmail.com>
2018-04-21 16:44:58 +0800

    add lifecycle parameter to s3 bucket

--- interface.tf
+++ interface.tf
@@ -12,3 +12,8 @@ variable "environment" {
  default = "development"
  description = "The name of our environment"
 }
+
+variable "state_bucket_lifecycle" {
+ default = "create_before_destroy"
+ description = "Lifecycle scheme for the S3 state bucket (one of: create_before_destroy, ignore_changes, prevent_destroy)"
+}
