8dadda84fe0b6037bda732c79bd40295365a8dc3
Traiano Welcome <traiano@gmail.com>
2018-04-21 17:08:44 +0800

    add output block for obtaining s3 id

--- interface.tf
+++ interface.tf
@@ -20,3 +20,7 @@ variable "state_bucket_lifecycle" {
  default = "create_before_destroy"
  description = "Lifecycle scheme for the S3 state bucket (one of: create_before_destroy, ignore_changes, prevent_destroy)"
 }
+
+output "s3_bucket_id" {
+ value = "${aws_s3_bucket.remote_state.id}"
+}
