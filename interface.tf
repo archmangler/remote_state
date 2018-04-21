3cb5f8b9d880b53fa052bb2ee1369555f31051b8
Traiano Welcome <traiano@gmail.com>
2018-04-21 16:18:40 +0800

    basic variables definition ofr s3 state bucket

---
+++ interface.tf
@@ -0,0 +1,14 @@
+variable "region" {
+ default = "us-east-1"
+ description = ""
+}
+
+variable "prefix" {
+ default = "engeneon"
+ description = "Organisation or Division Name"
+}
+
+variable "environment" {
+ default = "development"
+ description = "The name of our environment"
+}
